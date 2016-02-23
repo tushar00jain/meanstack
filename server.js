3b931c7190c307aab9ae161b92c471beeffbc3b4
tushar00jain <tushar00jain@ymail.com>
2016-02-23 15:14:54 +0800

    change send(html) to sendFile

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
 var routes = require('server/routes');
 
 var PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
+// production build
+app.use(express.static(path.join(__dirname, 'public')));
+
 routes(app);
 
+// production build
 app.all('/*', function(req, res) {
-	res.send('\
-	<!DOCTYPE html>\
-		<html>\
-			<head>\
-				<title>MEAN todo</title>\
-				<base href="/"/>\
-			</head>\
-			<body>\
-				<div ui-view></div>\
-				<script src="bundle.js"></script>\
-			</body>\
-		</html>\
-		\
-	');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, function() {
